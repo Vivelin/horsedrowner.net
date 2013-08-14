e7c2551e14ddcbcc0ba2b431b7d1e576f01bbdf8
Ruben Verdoes <horsedrowner@gmail.com>
2013-08-14 16:57:02 +0200

    Added another Starbound character

--- pages/characters.php
+++ pages/characters.php
@@ -11,5 +11,9 @@
 	<img src="/images/characters/starbound/human.png" alt="Human" class="pixelart">
 	<p>Human</p>
 </div>
+<div class="tile">
+	<img src="/images/characters/starbound/human2.png" alt="Human (2)" class="pixelart">
+	<p>Human (2)</p>
+</div>
 
 <div style="clear: both;"></div><!-- Fuck this. -->
