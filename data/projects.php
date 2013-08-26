056269ed4ecf032fffd0f16f43dc2c5a436db1d6
Ruben Verdoes <horsedrowner@gmail.com>
2013-08-26 19:54:04 +0200

    Separated project data and display

---
+++ data/projects.php
@@ -0,0 +1,29 @@
+<?php
+
+$projects = [
+	[
+		"name" => "Superscrot",
+		"description" => "A better screenshot uploader.",
+		"status" => "Passive development",
+		"version" => "1.6.4",
+		"download64" => "http://d.horsedrowner.net/Superscrot-1.6.4.zip",
+		"download32" => "http://d.horsedrowner.net/Superscrot-1.6.4-x86.zip",
+		"source" => "https://github.com/horsedrowner/Superscrot",
+	],
+	[
+		"name" => "Media Key Simulator",
+		"description" => "A small program that simulates media keys when pressing certain key combinations.",
+		"status" => "Completed",
+		"version" => "1.0",
+		"source" => "https://github.com/horsedrowner/MediaKeySimulator"
+	],
+	[
+		"name" => "Image to UML",
+		"description" => "This project is an assignment for the Software Engineering course at LIACS, Leiden University. The objective of the project is to develop tools for the conversion of images of UML class diagrams in common graphical formats (e.g., BMP, JPEG, GIF, etc.) to structured UML class diagrams stored e.g., in XMI format to enable their analysis with UML design tools. </p><p> The assignment was done in a group with <a hreflang=\"nl\" href=\"http://www.liacs.nl/~tkappe/\">Tobias Kapp&eacute;</a> and <a hreflang=\"nl\" href=\"http://www.liacs.nl/~plouchtc/\">Philipe Louchtch</a>.",
+		"status" => "Completed",
+		"source" => "http://code.google.com/p/image-to-uml/source/browse/#svn%2FLeiden%2FGroup6",
+		"customAttributes" => [
+			"Grade" => "9.5/10",
+		],
+	],
+];
--- pages/projects.php
+++ pages/projects.php
@@ -1,61 +1,59 @@
-<p>For a full list of projects, <a href="https://github.com/horsedrowner/">find me on GitHub</a>.</p>
+<?php
+function print_project($project) 
+{
+    $html = "<h1>" . htmlspecialchars($project["name"]) . "</h1>\n"
+          . "<p>" . $project["description"] . "</p>\n"
+          . "<dl>\n"
+          . "\t<dt>Status:\n\t<dd>" . htmlspecialchars($project["status"]) . "\n";
 
-<h1 id="superscrot">Superscrot</h1>
-<p>A better screenshot uploader.
-<dl>
-    <dt>Status:
-    <dd>Passive development
-    
-    <dt>Current version:
-    <dd><a href="https://github.com/horsedrowner/Superscrot/wiki/Version-History" target="_blank">1.6.4</a>
-</dl>
+    if (isset($project["version"])) {
+        $html .= "\t<dt>Version:\n"
+               . "\t<dd>" . htmlspecialchars($project["version"]) . "\n";
+    }
+
+    if (isset($project["customAttributes"])) {
+        foreach ($project["customAttributes"] as $key => $value) {
+            $html .= "\t<dt>" . htmlspecialchars($key) . ": \n"
+                   . "\t<dd>" . htmlspecialchars($value) . "\n";
+        }
+    }
+
+    /* URL-encoding and array checking... woo. */
+    $dl64 = "";
+    if (isset($project["download64"]))
+        $dl64 = " href=\"" . htmlspecialchars($project["download64"]) . "\"";
+
+    $dl32 = "";
+    if (isset($project["download32"]))
+        $dl32 = " href=\"" . htmlspecialchars($project["download32"]) . "\"";
+
+    $source = htmlspecialchars($project["source"]);
+    if (strpos($source, "github.com") !== FALSE) {
+        $sourceText = "Follow on Github";
+    } else {
+        $sourceText = "Source";
+    }
+
+    $html .= "</dl>\n"
+           . "<p>\n"
+           . "\t<a class=\"button\"" . $dl64 . ">Download (64-bit)</a>\n"
+           . "\t<a class=\"button\"" . $dl32 . ">Download (32-bit)</a>\n"
+           . "\t<a target=\"_blank\" href=\"" . $source . "\">" . $sourceText . "</a>\n"
+           . "</p>";
+
+    $html .= "\n";
+    echo $html;
+}
+
+include "data/projects.php";
+?> 
+
 <p>
-    <a class="button" href="http://d.horsedrowner.net/Superscrot-1.6.4.zip">Download (64-bit)</a>
-    <a class="button" href="http://d.horsedrowner.net/Superscrot-1.6.4-x86.zip">Download (32-bit)</a>
-    <a href="https://github.com/horsedrowner/Superscrot" target="_blank">Follow on GitHub</a>
+    For a full list of projects, <a href="https://github.com/horsedrowner/">find me on GitHub</a>.
 </p>
 
-<h1 id="tits">TITS</h1>
-<p>A music player that something something.</p>
-<dl>
-    <dt>Status:
-    <dd>"Active" development (with <a href="https://github.com/PhilipeLouchtch">Philipe Louchtch</a>)
-
-    <dt>Current version:
-    <dd>None (<a href="tits">View planned features</a>)
-</dl>
-<p>
-    <a href="https://github.com/horsedrowner/TITS" target="_blank">Follow on GitHub</a>
-</p>
-
-<h1 id="mediakeysimulator">Media Key Simulator</h1>
-<p>A small program that simulates media keys when pressing certain key combinations.
-<dl>
-    <dt>Status:
-    <dd>Completed
-    
-    <dt>Current version:
-    <dd>1.0
-</dl>
-<p>
-    <a class="button" href="http://d.horsedrowner.net/MediaKeySimulator.zip">Download (64-bit)</a>
-    <a class="button">Download (32-bit)</a>
-    <a href="https://github.com/horsedrowner/MediaKeySimulator" target="_blank">Follow on GitHub</a>
-
-<h1 id="image-to-uml">Image to UML</h1>
-<p>
-    This project is an assignment for the Software Engineering course at LIACS, Leiden University. The
-    objective of the project is to develop tools for the conversion of images of UML class diagrams in
-    common graphical formats (e.g., BMP, JPEG, GIF, etc.) to structured UML class diagrams stored e.g.,
-    in XMI format to enable their analysis with UML design tools.
-<p>
-    The assignment was done in a group with <a hreflang="nl" href="http://www.liacs.nl/~tkappe/">Tobias 
-    Kapp&eacute;</a> and <a hreflang="nl" href="http://www.liacs.nl/~plouchtc/">Philipe Louchtch</a>.
-<dl>
-    <dt>Status:
-    <dd>Completed
-
-    <dt>Grade:
-    <dd>9.5/10
-</dl>
-<p><a href="http://code.google.com/p/image-to-uml/source/browse/#svn%2FLeiden%2FGroup6" target="_blank">Google Code repository</a>
+<?php
+foreach ($projects as $project) {
+    print_project($project);
+}
+?> 
