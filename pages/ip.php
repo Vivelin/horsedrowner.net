3dd6c5e66ddb200b4f72fdcf19455569daaba885
Ruben Verdoes <horsedrowner@gmail.com>
2014-02-04 21:57:34 +0100

    Show actual IP

--- pages/ip.php
+++ pages/ip.php
@@ -1,6 +1,13 @@
+<?php
+$ip = $_SERVER["REMOTE_ADDR"];
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"]))
+	$ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+else if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+	$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+?>
 <dl>
     <dt>IP address
-    <dd><?php echo $_SERVER["REMOTE_ADDR"];?><em>:<?php echo $_SERVER["REMOTE_PORT"];?></em>
+    <dd><?php echo $ip;?><em>  	
 
     <dt>User agent
     <dd><?php echo $_SERVER["HTTP_USER_AGENT"];?> 
