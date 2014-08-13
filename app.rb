523dde1ab0202783920589be50e121fa4157411a
Ruben Verdoes <horsedrowner@gmail.com>
2014-08-13 12:13:49 +0200

    Override find_template instead of file.read

--- app.rb
+++ app.rb
@@ -4,19 +4,25 @@ require "sass"
 require "redcarpet"
 
 configure do
+    set :views, {
+        :markdown => 'pages',
+        :sass => 'styles',
+        :default => 'views'
+    }
+    set :markdown, {
+        :no_intra_emphasis  => true, # no_emphasis_here
+        :tables             => true,
+        :fenced_code_blocks => true, # Parses blocks surrounded by ``` as code
+        :autolink           => true,
+        :strikethrough      => true, # ~~strikethrough~~
+        :lax_spacing        => true, # Don't require blank lines around HTML blocks
+        :superscript        => true, # super^(script)
+        :quote              => true, # Parse "quotes" as <q>quotes</q>
+        :footnotes          => true, # Parse footnotes[^1]
+    }
+
     set :avatar, "/images/avatars/curly-512px.jpg"
     set :pages, ["about", "projects", "quotes"]
-    set :markdown, {
-        :no_intra_emphasis => true, # no_emphasis_here
-        :tables => true,
-        :fenced_code_blocks => true, # Parses blocks surrounded by ``` as code
-        :autolink => true,
-        :strikethrough => true, # ~~strikethrough~~
-        :lax_spacing => true, # Don't require blank lines around HTML blocks
-        :superscript => true, # super^(script)
-        :quote => true, # Parse "quotes" as <q>quotes</q>
-        :footnotes => true, # Parse footnotes[^1]
-    }
 end
 
 configure :development do
@@ -24,15 +30,21 @@ configure :development do
     BetterErrors.application_root = __dir__
 end
 
+helpers do
+    def find_template(views, name, engine, &block)
+        _, folder = views.detect { |k,v| engine == Tilt[k] }
+        folder ||= views[:default]
+        super(folder, name, engine, &block)
+    end
+end
+
 get "/style.css" do
-    sass :style
+    sass :main
 end
 
 get "/:page?" do
     @page = params[:page] || "about"
-    filename = "pages/#{ @page }.md"
-    content = File.read(filename).force_encoding "utf-8"
     erb :main_layout, :layout => false do
-        markdown content
+        markdown @page.to_sym
     end
 end
