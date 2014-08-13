77484fcbfc6af59bf6dba8566222987d9fa3724c
Ruben Verdoes <horsedrowner@gmail.com>
2014-08-13 15:29:46 +0200

    Set last modified date

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require "redcarpet"
 
 configure do
     set :views, {
-        :markdown => 'pages',
-        :sass => 'styles',
-        :default => 'views'
+        :markdown => "pages",
+        :sass => "styles",
+        :default => "views"
     }
     set :markdown, {
         :no_intra_emphasis  => true, # no_emphasis_here
@@ -31,19 +31,48 @@ configure :development do
 end
 
 helpers do
+    ##
+    # Overrides find_template to use different directories for different engines
+    #
     def find_template(views, name, engine, &block)
         _, folder = views.detect { |k,v| engine == Tilt[k] }
         folder ||= views[:default]
         super(folder, name, engine, &block)
     end
+
+    def last_modified_date(name, engine)
+        find_template settings.views, name, engine do |file|
+            return File.mtime(file) if File.exists?(file)
+        end
+        Time.now
+    end
+
+    def page_modified(name)
+        [
+            last_modified_date(name, Tilt[:markdown]),
+            last_modified_date(:main_layout, Tilt[:erb]),
+            last_modified_date(:footer, Tilt[:erb])
+        ].max
+    end
+
+    def style_modified(name)
+        last_modified_date(name, Tilt[:sass])
+    end
 end
 
 get "/style.css" do
+    last_modified style_modified(:main)
     sass :main
+end
+
+get "/ip" do
+    erb :ip_info, :layout => :main_layout
 end
 
 get "/:page?" do
     @page = params[:page] || "about"
+    last_modified page_modified(@page)
+
     erb :main_layout, :layout => false do
         markdown @page.to_sym
     end
