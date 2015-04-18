0bb3649e946a6330209f5c25f9fc661f30a0c144
Ruben Verdoes <horsedrowner@gmail.com>
2015-04-18 15:03:22 +0200

    I think this'll make for a good start

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'haml'
+gem 'kramdown'
+gem 'sass'
+
+group :development do
+    gem 'better_errors'
+    gem 'binding_of_caller'
+end
---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'sinatra/base'
+require 'better_errors'
+require 'tilt/haml'
+require 'tilt/sass'
+require 'tilt/kramdown'
+require 'yaml'
+
+class App < Sinatra::Base
+    configure :development do
+      use BetterErrors::Middleware
+      BetterErrors.application_root = __dir__
+    end
+
+    configure do
+      set :haml, escape_html: true
+      set :sass, views: 'styles'
+      set :markdown, views: 'pages', layout_options: { views: 'views' },
+                     layout_engine: :haml, smartypants: true
+
+      set :hersir_names, YAML.load_file('data/hersir.yml')
+      set :avatar, '/images/avatars/curly.jpg'
+    end
+
+    not_found do
+      haml :not_found
+    end
+
+    get '/style.css' do
+      sass :main
+    end
+
+    get '/name' do
+      headers 'Content-Type' => 'text/plain'
+      
+      first_name = settings.hersir_names['first_names'].sample
+      surname_first = settings.hersir_names['surnames_first'].sample
+      surname_second = settings.hersir_names['surnames_second'].sample
+      "#{ first_name } #{ surname_first }#{ surname_second }"
+    end
+
+    get '/:page?' do
+      page = params[:page] || 'about'
+
+      begin
+        markdown page.to_sym
+      rescue Errno::ENOENT
+        halt 404
+      end
+    end
+end
